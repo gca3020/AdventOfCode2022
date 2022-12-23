cbc4ef21bc85095f8d76a37527d433160a6e1868
Geoff Amey <gca3020@users.noreply.github.com>
2022-12-23 11:44:07 -0500

    Add skeletons for Days 24 & 25

---
+++ day25/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file := "input_test"
+	//file := "input.txt"
+
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+	lines := make([]string, 0)
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+}
